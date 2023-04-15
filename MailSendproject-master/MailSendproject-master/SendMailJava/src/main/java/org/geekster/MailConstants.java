f1a95a8a1b66fb21261a1f8c98ce58f35054046b
Priya kumari <113836711+PRiya310194@users.noreply.github.com>
2023-04-15 20:57:39 +0700

    Add files via upload

---
+++ MailSendproject-master/MailSendproject-master/SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,15 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "pk059257@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "pk059257@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+}
